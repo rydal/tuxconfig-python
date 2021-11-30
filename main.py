f527cc293903aae4cbc0ada2d145dfed95852a56
Roblet <rydalinc.uk@gmail.com>
2021-11-30 15:07:38 +0000

    woop!

--- main.py
+++ main.py
@@ -1,189 +1,75 @@
-import base64
-import io
 import json
 import os
 import platform
 import random
 import string
 import subprocess
-import threading
-import time
-import tkinter as Tk
 import urllib
 import webbrowser
-from functools import partial
 from json import JSONDecodeError
-from tkinter import ttk, GROOVE, LEFT, W, E
-from tkinter.ttk import Label
-from urllib.request import Request, urlopen
 
+
+import wx
 import requests
-from PIL import ImageTk, Image
-
-devices_installable = []
-class InstallProgress():
-    @staticmethod
-    def add_device(device):
-        devices_installable.append(device)
+from wx import ID_ANY, DefaultPosition, DefaultSize, DefaultValidator
+from wx.ribbon import RIBBON_BAR_DEFAULT_STYLE
 
 
 
 
-def set_device(device,tabControl):
-    InstallProgress.device = device
-    tabControl.select(2)
+def run_install(device):
 
-def show_devices_page(tabControl):
-    myframe = Tk.Frame(tabControl, relief=GROOVE, width=50, height=100, bd=1)
-
-    row_number = 1
-    title_label = ttk.Label(myframe, text="PCI devices", font=("Arial", 16))
-    title_label.grid()
-
-    for device in find_device_ids("pci").values():
-
-        device_label = ttk.Label(myframe, text=device.getString(), justify=LEFT, anchor="w")
-        device_label.grid(row=row_number, column=0, sticky=W)
-
-        selected_device, error = device.get_repository_details(device.vendor_id, device.device_id)
-        if selected_device.available:
-            InstallProgress.add_device(device)
-            button = Tk.Label(myframe, text="installable", justify=LEFT, anchor="w")
-            button.grid(row=row_number, column=1, sticky=E)
-        row_number += 1
-
-    title_label = ttk.Label(myframe, text="USB devices", font=("Arial", 16))
-    title_label.grid()
-    row_number += 1
-    for device in find_device_ids("usb").values():
-
-        device_label = ttk.Label(myframe, text=device.getString(), justify=LEFT, anchor="w")
-        device_label.grid(row=row_number, column=0, sticky=W)
-        selected_device, error = device.get_repository_details(device.vendor_id, device.device_id)
-
-        if selected_device.available:
-            button = Tk.Label(myframe, text="installable", justify=LEFT, anchor="w")
-            InstallProgress.add_device(device)
-            button.grid(row=row_number, column=1, sticky=E)
-        row_number += 1
-
-    return myframe
+    directory = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    clone_git = subprocess.Popen(["git", "clone", device.clone_url, "/tmp/" + directory], stdout=subprocess.PIPE)
+    streamdata = clone_git.communicate()[0]
+    if clone_git.returncode > 0:
+        print( "Error cloning git repo")
+        return
 
 
-def callback(url):
-    webbrowser.open_new(url)
+    set_git_commit = subprocess.Popen(["git", "reset", "--hard", device.commit], cwd='/tmp/' + directory,
+                                      stdout=subprocess.PIPE)
+    streamdata = set_git_commit.communicate()[0]
+    if set_git_commit.returncode > 0:
+        print( "Error setting Git branch")
 
+    if not os.path.exists("/tmp/" + directory + "/tuxconfig.conf"):
+        print( "Tuxconfig file not in directory")
+        return
 
-class WebImage:
-    def __init__(self, url):
-        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-        headers = {'User-Agent': user_agent, }
-        req = urllib.request.Request(url, None, headers)
+    install_command = None
+    test_command = None
 
-        self.image = Image.open(req)
+    with open("/tmp/" + directory + "/tuxconfig.conf", encoding="UTF-8") as tuxconfig_file:
+        lines = tuxconfig_file.readlines()
+        for index, line in enumerate(lines):
+            line = line.replace("\n", "")
+            line = line.replace("\"", "")
+            if "install_command" in line:
+                line.split("=")
+                install_command = line.split("=")[1].strip("\"")
+            if "test_command" in line:
+                line.split("=")
+                test_command = line.split("=")[1].strip("\"")
 
-    def get(self):
-        return self.image
+    if install_command.startswith("./"):
+        install_command = install_command[2:len(install_command)]
+    install_module = subprocess.Popen(["sudo", "/tmp/" + directory + "/" + install_command], stdout=subprocess.PIPE,
+                                      cwd="/tmp/" + directory)
+    streamdata = install_module.communicate()[0]
 
+    if install_module.returncode > 0:
+        print( "Error installing module")
+        return
 
+    test_module = subprocess.Popen(["bash",test_command], stdout=subprocess.PIPE,
+                                   cwd="/tmp/" + directory)
+    streamdata = test_module.communicate()[0]
+    if install_module.returncode > 0:
+        print( "Error testing module")
+        return
 
-
-
-def show_index_page(tabControl):
-    tab1 = ttk.Frame(tabControl)
-
-    title_label = ttk.Label(tab1, text="Tuxconfig - linux device installer", font=("Arial", 20))
-    title_label.pack()
-
-    link1 = Tk.Label(tab1, text="About Tuxconfig", fg="blue", cursor="hand2", font=("Arial", 20))
-    link1.pack()
-    link1.bind("<Button-1>", lambda e: callback("https://www.tuxconfig.com/about"))
-    link1 = Tk.Label(tab1, text="Developer site for contributing repositories", fg="blue", cursor="hand2",
-                     font=("Arial", 20))
-    link1.pack()
-    link1.bind("<Button-1>", lambda e: callback("https://www.tuxconfig.com/developer"))
-    title_label = ttk.Label(tab1,
-                            text="Install devices easily using this front end, covers usb and pci devices on x86, i386 and raspberry pi devices",
-                            font=("Arial", 16))
-    title_label.pack()
-
-    return tab1
-
-
-import tkinter as tk  ## Python 3.x
-
-
-class show_install_page(ttk.Frame):
-
-    def __init__(self,tabControl):
-        super().__init__()
-
-        self.labelA = ttk.Label(self, text="Tuxconfig - install device")
-        self.labelA.grid(column=1, row=1)
-        button = Tk.Button(self, command=lambda: self.run_install(),
-                           text="install device")
-        button.grid(column=1,row=2 )
-        for device in devices_installable:
-            link1 = Tk.Label(self, text="Install " + device.getString() , fg="blue", cursor="hand2", font=("Arial", 20))
-            link1.grid()
-
-
-
-
-    def run_install(self,device):
-        self.clone_url = device.clone_url
-        self.commit = device.commit
-        directory = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-        clone_git = subprocess.Popen(["git", "clone", self.clone_url, "/tmp/" + directory], stdout=subprocess.PIPE)
-        streamdata = clone_git.communicate()[0]
-        if clone_git.returncode > 0:
-            InstallProgress.error = "Error cloning git repo"
-            return
-
-        set_git_commit = subprocess.Popen(["git", "reset", "--hard", self.commit], cwd='/tmp/' + directory,
-                                          stdout=subprocess.PIPE)
-        streamdata = set_git_commit.communicate()[0]
-        if set_git_commit.returncode > 0:
-            InstallProgress.error = "Error setting Git branch"
-            return
-
-        if not os.path.exists("/tmp/" + directory + "/tuxconfig.conf"):
-            InstallProgress.error = "Tuxconfig file not in directory"
-            return
-        install_command = None
-        test_command = None
-
-        with open("/tmp/" + directory + "/tuxconfig.conf", encoding="UTF-8") as tuxconfig_file:
-            lines = tuxconfig_file.readlines()
-            for index, line in enumerate(lines):
-                line = line.replace("\n", "")
-                line = line.replace("\"", "")
-                if "install_command" in line:
-                    line.split("=")
-                    install_command = line.split("=")[1].strip("\"")
-                if "test_command" in line:
-                    line.split("=")
-                    test_command = line.split("=")[1].strip("\"")
-
-        if install_command.startswith("./"):
-            install_command = install_command[2:len(install_command)]
-        install_module = subprocess.Popen(["sudo", "/tmp/" + directory + "/" + install_command], stdout=subprocess.PIPE,
-                                          cwd="/tmp/" + directory)
-        streamdata = install_module.communicate()[0]
-
-        print(install_module.returncode)
-        if install_module.returncode > 0:
-            InstallProgress.error = "Error installing module"
-            return
-
-        test_module = subprocess.Popen(["/tmp/" + directory + "/" + test_command], stdout=subprocess.PIPE,
-                                       cwd="/tmp/" + directory)
-        streamdata = test_module.communicate()[0]
-        if install_module.returncode > 0:
-            InstallProgress.error = "Error testing module"
-            return
-        InstallProgress.status = "Test command run, module marked as working"
-        return True
+    return "Module installed!"
 
 
 def find_device_ids(type):
@@ -273,6 +159,7 @@ def get_platform():
 
 
 class device_details:
+
     def setName(self, device_name):
         self.device_name = device_name
 
@@ -294,14 +181,14 @@ class device_details:
     def setSubsystem(self, subsystem):
         self.subsystem = subsystem
 
-    def get_repository_details(self, vendor_id, device_id):
+    def get_repository_details(self ):
         current_repository = None
         response = requests.get(
-            'https://www.tuxconfig.com/user/get_device/' + vendor_id + ":" + device_id + "/" + get_platform())
+            'https://www.tuxconfig.com/user/get_device/' + self.vendor_id + ":" + self.device_id + "/" + get_platform())
         if response.status_code >= 400 and response.status_code < 400:
-            return None, "connection error"
+            return "connection error"
         elif response.status_code >= 500:
-            return None, "server error"
+            return  "server error"
         else:
             try:
                 string = response.content.decode('utf-8')
@@ -312,7 +199,7 @@ class device_details:
                 else:
                     if len(json_response) == 0:
                         print(
-                            'https://www.tuxconfig.com/user/get_device/' + vendor_id + ":" + device_id + "/" + get_platform())
+                            'https://www.tuxconfig.com/user/get_device/' + self.vendor_id + ":" + self.device_id + "/" + get_platform())
                         return None, "empty list"
                     current_repository = json_response[0]
                     for repository in json_response:
@@ -325,7 +212,6 @@ class device_details:
                         self.pk = current_repository['pk']
             except JSONDecodeError:
                 return "cannot parse server request"
-        return self, None
 
     def write_to_file(self):
 
@@ -360,7 +246,7 @@ class device_details:
         else:
             self.installed = "not installed"
 
-        return self.vendor_id + ":" + self.device_id + self.device_vendor + self.device_name
+        return self.vendor_id + ":" + self.device_id + " " +  self.device_vendor + " "+ self.device_name
 
     def getAvailable(self):
         return self.available
@@ -372,25 +258,43 @@ class device_details:
 notebook = None
 
 
-class MainApplication(tk.Tk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Tuxconfig")
-
-        self.notebook = ttk.Notebook(self)
-
-        self.Frame1 = show_index_page(self.notebook)
-        self.Frame2 = show_devices_page(self.notebook)
-        self.Frame3 = show_install_page(self.notebook)
-
-        self.notebook.add(self.Frame1, text='Index page')
-        self.notebook.add(self.Frame2, text='List devices')
-        self.notebook.add(self.Frame3, text='Install devices')
-
-        self.notebook.pack()
-
 
 if __name__ == '__main__':
-    app = MainApplication()
-    app.mainloop()
+    devices = []
+    pci_devices = find_device_ids("pci")
+    devices.append(pci_devices)
+    usb_devices = find_device_ids("usb")
+    devices.append(usb_devices)
+    index = 0
+    device_map = []
+    for device in devices:
+        for a in device.values():
+            a.get_repository_details()
+            if a.available:
+                device_map.append( a)
+                index += 1
+                print(str(index) + ") " + a.getString() + "installable from Tuxconfig.com")
+
+    install_number = input("Install device from list: ")   # Python 3
+    install_number_int = 1
+    try:
+        install_number_int = int(install_number)
+    except:
+        print("Not a valid choice number")
+    install_result = False
+    device_to_install = None
+    if int(install_number) > len(device_map):
+        print("Not a valid choice number")
+    else:
+        device_to_install = device_map[install_number_int -1 ]
+        install_result = run_install(device_to_install)
+    if install_result is True:
+        print("https://www.tuxconfig.com/user/get_contiributor/" + device_to_install.pk )
+    else:
+        print ("install failed")
+
+
+
+
+
+
